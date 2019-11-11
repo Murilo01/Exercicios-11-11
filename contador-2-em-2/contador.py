08d9927274e3ce4a68e7b54f15ef40bc54f32a24
Murilo <muriloduarte722@gmail.com>
2019-11-11 21:35:12 -0200

    commit python

---
+++ contador-2-em-2/contador.py
@@ -0,0 +1,6 @@
+contador = 0
+
+while contador <= 1000:
+    print(contador)
+    contador = contador + 2
+
---
+++ numeros-pares/aula-numeros-pares.py
@@ -0,0 +1,8 @@
+numero = int(input("Entre com um número inteiro: "))
+
+if numero % 2 == 0:
+    print("O número", numero, "é Par")
+else:
+    print("O número", numero, "é Ímpar")
+
+
